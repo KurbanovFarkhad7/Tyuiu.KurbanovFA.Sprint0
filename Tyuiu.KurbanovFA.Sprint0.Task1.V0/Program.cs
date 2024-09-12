e3eb24092412626590362e7efd63ea1f32bbd968
Tyuiu.KurbanovFA@outlook.com <Tyuiu.KurbanovFA@outlook.com>
2024-09-12 22:25:45 +0500

    Task1

---
+++ Tyuiu.KurbanovFA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.KurbanovFA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Фархад";
+            int age = 23;
+            Console.WriteLine("Здравствуйте. Меня зовут "+name+" Мне "+age+ " лет");
+            Console.ReadKey();
+        }
+    }
+}
