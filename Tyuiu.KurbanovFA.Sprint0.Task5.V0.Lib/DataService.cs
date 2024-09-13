89c423351cbbaab31acaf09df63956dd110d4317
KurbanovFarkhad7 <Tyuiu.KurbanovFA@outlook.com>
2024-09-13 22:07:53 +0500

    Task5

---
+++ Tyuiu.KurbanovFA.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿namespace Tyuiu.KurbanovFA.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+    }
+}
---
+++ Tyuiu.KurbanovFA.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.KurbanovFA.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.KurbanovFA.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+            Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+            Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+            Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+
+            Console.ReadKey();
+        }
+    }
+}
