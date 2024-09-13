62ec04043280690aec35dc8b461e23cf6c423a61
Tyuiu.KurbanovFA@outlook.com <Tyuiu.KurbanovFA@outlook.com>
2024-09-13 21:17:24 +0500

    Task4

--- Tyuiu.KurbanovFA.Sprint0.Task4.V0/Program.cs
+++ Tyuiu.KurbanovFA.Sprint0.Task4.V0/Program.cs
@@ -10,7 +10,6 @@ namespace Tyuiu.KurbanovFA.Sprint0.Task4.V0
             Console.WriteLine(DataService.Subtraction(15, 5));
             Console.WriteLine(DataService.Multiplication(10, 10));
             Console.WriteLine(DataService.Division(5, 5));
-
             Console.ReadKey();
         }
     }
