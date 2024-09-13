48bbdf4b6249794ae8d12c5fc402ff87fe8f0ca9
KurbanovFarkhad7 <Tyuiu.KurbanovFA@outlook.com>
2024-09-13 21:50:34 +0500

    Task4

--- Tyuiu.KurbanovFA.Sprint0.Task4.V0.Lib/DataService.cs
+++ Tyuiu.KurbanovFA.Sprint0.Task4.V0.Lib/DataService.cs
@@ -6,14 +6,17 @@
         {
             return a + b;
         }
+
         public static int Subtraction(int a, int b)
         {
             return a - b;
         }
+
         public static int Multiplication(int a, int b)
         {
             return a * b;
         }
+
         public static int Division(int a, int b)
         {
             return a / b;
